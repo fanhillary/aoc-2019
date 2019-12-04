8cb6be7f7a61cc252e825811b1642fee7d1b5ac7
Hillary Fan <hillary.fan123@gmail.com>
2019-12-04 13:45:39 -0800

    working functions for finding number of valid passwords

--- aoc4.py
+++ aoc4.py
@@ -1,28 +1,31 @@
 def numValid(start, end):
     countValid = 0
     currNum = start
-    ifDuplicate = False
     if (start < 100000 and end > 100000):
         currNum = 100000
     
     while (currNum < end):
+        ifDuplicate = False
+        ifDecreasing = False
         currString = str(currNum)
-        for i in range(0, 4):
+        for i in range(0, 5):
             currInt = int(currString[i])
             nextInt = int(currString[i+1])
 
             # it is a decreasing number, so skip to equal number for remainder of integer
             if (currInt > nextInt): 
                 currNum = int(populateRest(currString, i))
+                ifDecreasing = True
                 break
             if (currInt == nextInt): 
                 ifDuplicate = True
 
-        if ifDuplicate:
-            countValid = countValid + 1
+        if not ifDecreasing:
+            if ifDuplicate:
+                countValid = countValid + 1
+                print(currNum)
 
-        ifDuplicate = False
-        currNum = currNum + 1
+            currNum = currNum + 1
     return countValid
 
             
