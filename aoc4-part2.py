b666769adcf76b2627320d8f9b2d1f09c70ce12e
Hillary Fan <hillary.fan123@gmail.com>
2019-12-05 14:06:58 -0800

    Attempted part 2 of day 4, added comments to aoc4

--- aoc4-part2.py
+++ aoc4-part2.py
@@ -1,6 +1,7 @@
 # Name: numValid(start, end)
-# Description: Finds amount of number in range(start,end) that have 6 digits, only increasing or equal digits, and also have adjacent duplicates
-# ex) valid numbers: 111111, 133459. invalid numbers: 123456, 944234
+# Description: Finds amount of number in range(start,end) that have 6 digits, only increasing or equal digits, 
+#              and also have multiple adjacent duplicates
+# ex) valid numbers: 112222, 133449. invalid numbers: 111111(adjacent duplicate is part of large group of adjacents), 123456, 944234
 # Params: start: int - starting bound for range of number to check
 #         end: int - end bound for range of numbers to check
 # returns: amount of numbers that match the criteria within the range
@@ -10,28 +11,37 @@ def numValid(start, end):
     if (start < 100000 and end > 100000):
         currNum = 100000
     
-    while (currNum < end):
-        ifDuplicate = False
+    while (currNum <= end):
         ifDecreasing = False
+        adjacentCount = [0] * 10
         currString = str(currNum)
         for i in range(0, 5):
-            currInt = int(currString[i])
-            nextInt = int(currString[i+1])
+            currDigit = int(currString[i])
+            nextDigit = int(currString[i+1])
 
             # it is a decreasing number, so skip to equal number for remainder of integer
-            if (currInt > nextInt): 
+            if (currDigit > nextDigit): 
                 currNum = int(populateRest(currString, i))
                 ifDecreasing = True
+                print('currNum: ' + str(currNum))
                 break
-            if (currInt == nextInt): 
-                ifDuplicate = True
+            if (currDigit == nextDigit): 
+                if not adjacentCount[currDigit]:
+                    adjacentCount[currDigit] = 1
+                adjacentCount[currDigit] += 1 # counting the number of adjacent numbers in a row
+                print(adjacentCount)
 
         if not ifDecreasing:
-            if ifDuplicate:
-                countValid = countValid + 1
+            numDuplicates = 0
+            for count in adjacentCount:
+                if (count > 1):
+                    numDuplicates += 1
+
+            if numDuplicates > 1:
+                countValid += 1
                 print(currNum)
 
-            currNum = currNum + 1
+            currNum += 1
     return countValid
 
             
@@ -52,7 +62,7 @@ def populateRest(numString, index):
 
 result = numValid(152085,670283)
 
-# result = numValid(152085,157000)
+# result = numValid(152085,156000)
 print(result)
 # populateRest("152085", 1)
         
